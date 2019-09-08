97d551e536bc102522de2cb797a4b5f0879bf6fa
Abed Islam <aislam360@gmail.com>
2019-09-08 11:20:56 -0400

    First commit

---
+++ src/Lab2Part2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Lab2Part2 {
+
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		double sharePrice,  Number_of_shares, Purchase_price, Sale_price, commissionTotal, Purchase_commission, Sale_commission, net_Price;
+		
+		
+	}
+}
