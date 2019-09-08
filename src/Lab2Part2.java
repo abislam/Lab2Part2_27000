a5c63e07c32b91ebd0538fd3000153860b67caa8
Abed Islam <aislam360@gmail.com>
2019-09-08 12:05:00 -0400

    added user input functionality and formulae for calculations

--- src/Lab2Part2.java
+++ src/Lab2Part2.java
@@ -6,12 +6,33 @@ public class Lab2Part2 {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
-		double sharePrice,  Number_of_shares, Purchase_price, Sale_price, commissionTotal, Purchase_commission, Sale_commission, net_Price;
+		double sharePrice,  Number_of_shares, Purchase_price, Sale_price, commissionTotal, Purchase_commission; 
+		double Sale_commission, net_Price;
 		
+
+		//user input to get values
 		System.out.print("Shares: ");
+		Number_of_shares = input.nextDouble();
+		
 		System.out.print("Purchase Price: ");
+		Purchase_price = input.nextDouble();
+		
 		System.out.print("Purchase Commission: ");
+		Purchase_commission = input.nextDouble();
+		
 		System.out.print("Sale Price: ");
+		Sale_price = input.nextDouble();
+		
 		System.out.print("Sale commission: ");
+		Sale_commission = input.nextDouble();
+		
+		//close scanner
+		input.close();
+		
+		//formulae
+		sharePrice = (Number_of_shares * Purchase_price ) - (Number_of_shares * Sale_price);
+		commissionTotal = (Number_of_shares * Purchase_price * Purchase_commission )+ (Number_of_shares * Sale_price * Sale_commission );
+		net_Price = commissionTotal + sharePrice;
+		
 	}
 }
