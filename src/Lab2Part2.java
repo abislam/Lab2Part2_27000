d7c739fa318af47ecbb8cdf189e80b748497a7ce
Abed Islam <aislam360@gmail.com>
2019-09-08 11:58:32 -0400

    Changed print line statements

--- src/Lab2Part2.java
+++ src/Lab2Part2.java
@@ -8,10 +8,10 @@ public class Lab2Part2 {
 		Scanner input = new Scanner(System.in);
 		double sharePrice,  Number_of_shares, Purchase_price, Sale_price, commissionTotal, Purchase_commission, Sale_commission, net_Price;
 		
-		System.out.print("Number of Shares bought and sold ");
-		System.out.print("Purchase price per share: ");
-		System.out.print("Purchase commission: ");
-		System.out.print("Sale Price per share: ");
-		System.out.print("Sale commision: ");
+		System.out.print("Shares: ");
+		System.out.print("Purchase Price: ");
+		System.out.print("Purchase Commission: ");
+		System.out.print("Sale Price: ");
+		System.out.print("Sale commission: ");
 	}
 }
