12f8918a8ebc871ec795a960474a3eb0962ea214
Abed Islam <aislam360@gmail.com>
2019-09-08 11:27:55 -0400

    Added print lines

--- src/Lab2Part2.java
+++ src/Lab2Part2.java
@@ -8,6 +8,10 @@ public class Lab2Part2 {
 		Scanner input = new Scanner(System.in);
 		double sharePrice,  Number_of_shares, Purchase_price, Sale_price, commissionTotal, Purchase_commission, Sale_commission, net_Price;
 		
-		
+		System.out.print("Number of Shares bought and sold ");
+		System.out.print("Purchase price per share: ");
+		System.out.print("Purchase commission: ");
+		System.out.print("Sale Price per share: ");
+		System.out.print("Sale commision: ");
 	}
 }
